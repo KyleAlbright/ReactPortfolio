388107002e1b316604ea0929ece638810a87cdf4
Kyle Albright <kyle_albright@hotmail.com>
2023-03-09 20:48:54 -0600

    added last project to portfolio

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -56,9 +56,9 @@ const MyProjects = () => {
       <div className="card-image" id="card-image7">
         <img className = 'ReactLogo' src={ReactLogo} alt="React Logo" />
       </div>
-      <button className="card-button btn"><a href="https://github.com/KyleAlbright/ReactPortfolio"target="_blank>">Github</a></button>
-      <button className="card-button btn"><a href="https://kylealbright.github.io/ReactPortfolio/#/"target="_blank>">Application</a></button>
-      <h4>React Portfolio</h4>
+      <button className="card-button btn"><a href="https://github.com/KyleAlbright/BottomsUpOrdering"target="_blank>">Github</a></button>
+      <button className="card-button btn"><a href="https://bottoms-up-ordering.herokuapp.com/"target="_blank>">Application</a></button>
+      <h4>Mern Full-stack</h4>
       </div>
 
   
